67810964bc381fb3427f51ed558e75697152aa62
Adam Bausch <Fireworksanimations21@gmail.com>
2024-05-04 13:46:45 +0200

    paar oude dingetjes

--- coding/BankRekening.h
+++ coding/BankRekening.h
@@ -9,7 +9,6 @@ BankRekening() : date(""), amount(0.0), balance(0.0){}
     BankRekening(const std::string &d, double amt, double bal);
     
     void DisplayBankingInfo();
-    std::list<int> TransactieGeschiedenis;
     double balance, amount;
     std::vector<BankRekening> RekeningInfo;
     std::string date;
